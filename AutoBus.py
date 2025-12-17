f014dbe0caa522d1558f326ad0ec8448033a3da0
egamanflores94-sudo <megamanflores94@gmail.com>
2025-12-17 11:54:02 -0600

    Soy Max y cree el codigo de Autobus.py

---
+++ AutoBus.py
@@ -0,0 +1,17 @@
+def agencia_viajes():
+    destino = "Acapulco"
+    costo_pasajero = 420
+
+    print("AGENCIA DE VIAJES")
+    print("Destino",destino)
+    pasajeros = int(input("Ingresa el numero de pasajeros: "))
+
+    total_pagar = pasajeros + costo_pasajero
+
+    print("\nRESUMEN DEL VIAJE")
+    print("Destino:",destino)
+    print("Pasajeros:",pasajeros)
+    print("Costo por pasajero: $",costo_pasajero)
+    print("Total a pagar: $",total_pagar)
+
+agencia_viajes()
