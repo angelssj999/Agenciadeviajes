6fee9e760e1da6f733795f39564c8997ab9a4aa2
angelssj999 <angelcrux778@gmail.com>
2025-12-19 08:00:38 -0600

    Update avion.py

--- avion.py
+++ avion.py
@@ -1,5 +1,18 @@
 #programa sencillo de avion
-pasajeros=int(input("numero de pasajeros:"))
-precio_por_pasajeros=500
-total=pasajeros*precio_por_pasajeros
-print("total a pagar:$",total)
+def agencia_viajes():
+    destino = "Acapulco"
+    costo_pasajero = 500
+
+    print("AGENCIA DE VIAJES")
+    print("Destino",destino)
+    pasajeros = int(input("Ingresa el numero de pasajeros: "))
+
+    total_pagar = pasajeros + costo_pasajero
+
+    print("\nRESUMEN DEL VIAJE")
+    print("Destino:",destino)
+    print("Pasajeros:",pasajeros)
+    print("Costo por pasajero: $",costo_pasajero)
+    print("Total a pagar: $",total_pagar)
+
+agencia_viajes()
